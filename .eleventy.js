a0d68f7eba0c53fa1af148d229fd86452f8b5398
mightyplow <mightyplow@gmail.com>
2018-07-31 07:16:03 +0200

    pass options

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,7 @@ const addHashes = require('./lib/addHashes');
 module.exports = function (options) {
   return function(eleventyConfig, pluginNamespace) {
         eleventyConfig.namespace(pluginNamespace, () => {
-            eleventyConfig.addTransform('cacheBuster', addHashes('./dist'));
+            eleventyConfig.addTransform('cacheBuster', addHashes(options));
         });
     };
 };
