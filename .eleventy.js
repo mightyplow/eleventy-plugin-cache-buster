0de5122751f085d9a8949964b37474ff42856658
mightyplow <mightyplow@gmail.com>
2018-08-02 10:18:57 +0200

    determine output directory; refactor replacement logic

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,17 @@
+const minimist = require('minimist');
+
 const addHashes = require('./lib/addHashes');
+const getObjectProperty = require('./lib/getObjectProperty.js');
+
+const args = minimist(process.argv.slice(2));
+const defaultOutputDir = '_site';
 
 module.exports = function (options) {
-  return function(eleventyConfig, pluginNamespace) {
+    return function (eleventyConfig, pluginNamespace) {
+        const outputDirectory = args.output || getObjectProperty(eleventyConfig, 'dir', 'output') || defaultOutputDir;
+
         eleventyConfig.namespace(pluginNamespace, () => {
-            eleventyConfig.addTransform('cacheBuster', addHashes(options));
+            eleventyConfig.addTransform('cacheBuster', addHashes(outputDirectory, options));
         });
     };
 };
