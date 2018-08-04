7578834fa226a6a746c11f7632c729741b1642ec
mightyplow <mightyplow@gmail.com>
2018-08-04 08:28:32 +0200

    add jsdoc

--- src/createResourceFilename.js
+++ src/createResourceFilename.js
@@ -1,5 +1,13 @@
 const path = require('path');
 
+/**
+ * Creates the filename for a resource url depending on whether it's domain relative or path relative.
+ *
+ * @param {string} outputDir
+ * @param {string} target
+ * @param {string} url
+ * @return {string}
+ */
 module.exports = function createResourceFilename (outputDir, target, url) {
     if (url.charAt(0) === '/') {
         return path.join(outputDir, url);
